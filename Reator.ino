713ac560ead9dbdbd4dfbf1386c5391fedb4e601
JulianJose <julianjosedebrito@hotmail.com>
2019-05-30 21:49:04 -0300

    Adicionado: - Sensor de nível; - Controle proporcional na velocidade das bombas e da valvula de saída para volumes especificos; - Exibição de dados mais intuitiva; - Possibilidade de selecionar o tempo de exibição dos dados; - Acesso a atualização do volume pelo monitor serial.

--- Reator.ino
+++ Reator.ino
@@ -4,8 +4,9 @@
  *
  *  Autor: Julian Jose de Brito
  *
- *  Versão 1.94 29/04/2019: Controle da válvula ajustado.
- *                          Entrada das bombas e valvulas ajustadas para percentual.
+ *  Versão 2.1 30/05/2019: Controle proporcional de volume.
+ *                         Simplificação do código.
+ *                         Melhora na exibição de dados.
  * 
  */
 
@@ -16,63 +17,69 @@
 #include <OneWire.h> // ler a biblioteca
 #include <DallasTemperature.h> // ler a biblioteca
 #include <TimerFour.h>
+#include <HX711.h>
 
-//carrega a biblioteca para configurar as interrupções
-//#include <TimerThree.h>
 
 //#######################################################################################################################
 // DEFINIÇÕES DOS PINOS
 
-// Define pinos para o pwm das bombas.
-#define velocidade_bomba1 5
-#define velocidade_bomba2 6
-
-// Define o pino para a valvula de saída.
-#define valvula 8
-
-// Define o pino de saida para a resistência.
-#define resistencia 7
-
-// Sinal do DS18B20 (sensor de temperatura).
-#define sensor_temperatura 3
-
-// Define os pinos de saida para o motor de passo (agitador).
-#define step_agitador 10
-#define sentido_agitador 11
-#define sleep 12
+#define PWM_BOMB_1 5
+#define PWM_BOMB_2 6
+#define VALVULA 8
+#define RESISTENCIA 7
+#define SENSOR_TEMPERATURA 3
+#define STEP_AGITADOR 10
+#define SENTIDO_AGITADOR 11
+#define SLEEP 12
+#define PRESSAO_DOUT_PIN  A0
+#define PRESSAO_SCK_PIN  A1
+#define NIVEL A2
 
 //#######################################################################################################################
 // CONFIGURAÇÕES INICIAIS
 
 // Define uma instancia do oneWire para comunicacao com o sensor de temperatura
-OneWire oneWire(sensor_temperatura);
+OneWire oneWire(SENSOR_TEMPERATURA);
 DallasTemperature sensors(&oneWire);
 DeviceAddress ENDERECO_SENSOR_TEMPERATURA;
+
+// Define uma instância do sensor de pressão
+HX711 sensor_pressao;
 
 //#######################################################################################################################
 // PAINEL DE CONTROLE
 
 //Define as chaves de controle
-boolean CH_LIGA_BOMBA1 = 0;
-boolean CH_LIGA_BOMBA2 = 0;
-boolean CH_LIGA_VALVULA = 0;
-boolean CH_LIBERA_SENSOR_TEMPERATURA = 0;
-boolean CH_RESISTENCIA = 0;
+boolean CH_LIGA_BOMBA1 = 1;
+boolean CH_LIGA_BOMBA2 = 1;
+boolean CH_LIGA_VALVULA = 1;
+boolean CH_LIBERA_SENSOR_TEMPERATURA = 1;
+boolean CH_RESISTENCIA = 1;
 boolean CH_HABILITA_AGITADOR = 0;
+boolean CH_HABILITA_SENSOR_PRESSAO = 1;
+boolean CH_HABILITA_SENSOR_NIVEL = 0;
 
 //#######################################################################################################################
 // VARIÁVEIS DO SISTEMA
 
-float TEMPERATURA;
+float TEMPERATURA, VOLUME = 0, VOLUME_OBJETIVO = 0, Vi = 0, Ve = 0.0;
 int VELOCIDADE_AGITADOR = 100, 
     VELOCIDADE_AGITADOR_ANTERIOR = 0,
-    TEMPERATURA_RESISTENCIA = 50, 
+    TEMPERATURA_RESISTENCIA = 0, 
     PWM_BOMBA1 = 0, PWM_BOMBA2 = 0, 
     PERIODO_INTERVALO_AGITADOR = 0, 
     PERIODO_DESLIGADO = 0,
-    ABERTURA_VALVULA = 0; //100 Completamente fechada 
+    ABERTURA_VALVULA = 0,
+    TE = 2; // Responsável pelo tempo de exibição dos dados.
 
-unsigned long PERIODO_AGITADOR; // Variável que vai guardar o tempo dos intervalor de ativação do agitador
+long TEMPO;
+    
+boolean LEITURA_NIVEL = 0; 
+
+const long PRESSAO_OFFSET = 50682624;
+const long PRESSAO_DIVIDER = 5895655;
+
+float PERIODO_AGITADOR; // Variável que vai guardar o tempo dos intervalos de ativação do agitador
 boolean PERIODO = false; // Boolean dependente do perído definido.
 
 void setup()
@@ -86,26 +93,34 @@ void setup()
    sensors.getAddress(ENDERECO_SENSOR_TEMPERATURA, 0);
 
 
-   pinMode(velocidade_bomba1, OUTPUT);
-   digitalWrite(velocidade_bomba1, LOW);
+   pinMode(PWM_BOMB_1, OUTPUT);
+   digitalWrite(PWM_BOMB_1, LOW);
 
-   pinMode(velocidade_bomba2, OUTPUT);
-   digitalWrite(velocidade_bomba1, LOW);
+   pinMode(PWM_BOMB_2, OUTPUT);
+   digitalWrite(PWM_BOMB_1, LOW);
 
-   pinMode(valvula, OUTPUT);
-   digitalWrite(velocidade_bomba1, LOW);
+   pinMode(VALVULA, OUTPUT);
+   digitalWrite(PWM_BOMB_1, LOW);
 
-   pinMode(resistencia, OUTPUT);
-   digitalWrite(velocidade_bomba1, LOW);
+   pinMode(RESISTENCIA, OUTPUT);
+   digitalWrite(PWM_BOMB_1, LOW);
 
-   pinMode(step_agitador, OUTPUT);
-   digitalWrite(step_agitador, HIGH);
+   pinMode(STEP_AGITADOR, OUTPUT);
+   digitalWrite(STEP_AGITADOR, HIGH);
 
-   pinMode(sentido_agitador, OUTPUT);
-   digitalWrite(sentido_agitador, LOW);
+   pinMode(SENTIDO_AGITADOR, OUTPUT);
+   digitalWrite(SENTIDO_AGITADOR, LOW);
 
-   pinMode(sleep, OUTPUT);
-   digitalWrite(sleep, LOW);
+   pinMode(SLEEP, OUTPUT);
+   digitalWrite(SLEEP, LOW);
+
+   pinMode(NIVEL, INPUT);
+
+   sensor_pressao.begin(PRESSAO_DOUT_PIN, PRESSAO_SCK_PIN);
+   sensor_pressao.set_scale(PRESSAO_DIVIDER);
+   sensor_pressao.set_offset(PRESSAO_OFFSET);
+
+   sensor_pressao.tare();
 
 }
 
@@ -115,16 +130,72 @@ void loop()
   //verifica se algo foi digitado no canal serial
   le_informacao();
 
+  if(CH_HABILITA_SENSOR_NIVEL)
+  {
+    LEITURA_NIVEL = (analogRead(NIVEL) < 100) ? 1 : 0;
+  }
+
   if(CH_LIBERA_SENSOR_TEMPERATURA)
-  le_sensor_temperatura();
+    le_sensor_temperatura();
 
   if(CH_RESISTENCIA)
-  aciona_resistencia();
+    aciona_resistencia();
 
   if(CH_HABILITA_AGITADOR)
-  aciona_agitador();
+    aciona_agitador();
 
-  exibe_dados();
+  if(CH_HABILITA_SENSOR_PRESSAO)
+    le_sensor_pressao();
+
+  if(VOLUME_OBJETIVO != 0)
+  { 
+      if((VOLUME_OBJETIVO - VOLUME) < -0.01)
+      {
+        ABERTURA_VALVULA = (abs(VOLUME_OBJETIVO - VOLUME)/VOLUME_OBJETIVO)*100;
+        if(ABERTURA_VALVULA < 60)
+          ABERTURA_VALVULA = 60;
+          
+        muda_valvula();
+
+        desativa_bombas();
+      }
+      else if(((VOLUME_OBJETIVO - VOLUME) > 0.01) && !LEITURA_NIVEL)
+      {
+        ABERTURA_VALVULA = 0;
+        muda_valvula();
+
+        int delta = abs((VOLUME_OBJETIVO - VOLUME)/VOLUME_OBJETIVO)*100;
+        if(delta < 15)
+        {
+          PWM_BOMBA1 = 2;
+          PWM_BOMBA2 = 2;
+        }
+        else
+        {
+          PWM_BOMBA1 = delta;
+          PWM_BOMBA2 = delta;
+        }
+        
+        liga_bomba1();
+        liga_bomba2();
+      }
+      else if((abs(VOLUME_OBJETIVO - VOLUME) <= 0.02))
+      {
+        ABERTURA_VALVULA = 0;
+        muda_valvula();
+        
+        desativa_bombas();
+        VOLUME_OBJETIVO = 0;
+      }
+    
+  }
+
+  if((millis() - TEMPO)/1000 > TE)
+  {
+    exibe_dados();
+    TEMPO = millis();
+  }
+  
 }
 
 void le_informacao()
@@ -137,7 +208,7 @@ void le_informacao()
         case 'B':
                     if((Serial.available() > 0) && (Serial.read() == '1'))
                       PWM_BOMBA1 = Serial.parseInt();
-                    else//if((Serial.available() > 0) && (Serial.read() == '2'))
+                    else
                       PWM_BOMBA2 = Serial.parseInt();
 
                     if(CH_LIGA_BOMBA1) // Aplica as alterações 
@@ -163,10 +234,42 @@ void le_informacao()
                     ABERTURA_VALVULA = Serial.parseInt();
                     if(CH_LIGA_VALVULA)
                       muda_valvula();
-
                     break;
+        case 'L': 
+                    if(CH_HABILITA_SENSOR_PRESSAO)  //if((CH_LIGA_BOMBA1 || CH_LIGA_BOMBA2) && (CH_LIGA_VALVULA))
+                    {
+                      VOLUME_OBJETIVO = Serial.parseFloat();
+                      Serial.print("Volume objetivo: ");
+                      Serial.println(VOLUME_OBJETIVO);
+                    }
+                    if(LEITURA_NIVEL && VOLUME_OBJETIVO < 5)
+                    {
+                      
+                    }
+                    break;
+        case 'S':
+                    if(Serial.available() > 0)
+                    {
+                      switch (Serial.read())
+                      {
+                          case 'i':
+                                    Vi = Serial.parseFloat();
+                                    Serial.println("Volume Ajustado!");
+                                    break;
+                          case 'v': 
+                                    sensor_pressao.tare();
+                                    Serial.println("Volume Zerado!");
+                                    break;
+                          case 'E':
+                                    TE = Serial.parseInt();
+                                    Serial.println("Tempo de exibicao ajustado!");
+                                    break;
+
+                      }
+                    }
+
         default:
-                    Serial.println("Opção inválida!");
+                    //Serial.println("Opção inválida!");
                     break;
       }
    }
@@ -175,18 +278,25 @@ void le_informacao()
 
 void liga_bomba1()
 {
-  analogWrite(velocidade_bomba1, map(PWM_BOMBA1,0,100,0,253));
+    analogWrite(PWM_BOMB_1, map(PWM_BOMBA1,0,100,0,253));
 }
 
 void liga_bomba2()
 {
-    analogWrite(velocidade_bomba2, map(PWM_BOMBA2,0,100,0,253));  
+    analogWrite(PWM_BOMB_2, map(PWM_BOMBA2,0,100,0,253));  
+}
+
+void desativa_bombas()
+{
+        PWM_BOMBA1 = 0;
+        PWM_BOMBA2 = 0;
+        liga_bomba1();
+        liga_bomba2();
 }
 
 void muda_valvula()
 {
-    // Regular a valvula!
-    analogWrite(valvula, map(ABERTURA_VALVULA, 0, 100, 100, 225));  
+   analogWrite(VALVULA, map(ABERTURA_VALVULA, 0, 100, 115, 225));  
 }
 
 void le_sensor_temperatura()
@@ -195,15 +305,25 @@ void le_sensor_temperatura()
   TEMPERATURA = sensors.getTempC(ENDERECO_SENSOR_TEMPERATURA);
 }
 
+void le_sensor_pressao()
+{
+    VOLUME = (sensor_pressao.get_units()*25.795 + Vi);
+   
+    //if(sensor_pressao.is_ready())
+    //  VOLUME = (sensor_pressao.read()*0.00000353+Vi-Ve);
+     //if(sensor_pressao.is_ready())
+        //VOLUME = (sensor_pressao.read()*0.00000353+Vi-Ve);
+}
+
 void aciona_resistencia()
 {
   // Verifica se a temperatura desejada é maior ou menor do que a temperatura de leitura do sensor
 
   if(TEMPERATURA > TEMPERATURA_RESISTENCIA)
-  digitalWrite(resistencia, LOW);
+  digitalWrite(RESISTENCIA, LOW);
 
   if(TEMPERATURA < TEMPERATURA_RESISTENCIA)
-  digitalWrite(resistencia, HIGH);
+  digitalWrite(RESISTENCIA, HIGH);
 }
 
 void aciona_agitador()
@@ -244,59 +364,70 @@ void aciona_agitador()
   
   if((VELOCIDADE_AGITADOR == 0) || (PERIODO == 0 && PERIODO_INTERVALO_AGITADOR != 0))
   {
-      noTone(step_agitador);
-      digitalWrite(sleep, LOW);
+      noTone(STEP_AGITADOR);
+      digitalWrite(SLEEP, LOW);
   }
   else if((VELOCIDADE_AGITADOR != 0) && PERIODO)
   {
-      digitalWrite(sleep, HIGH);
-      tone(step_agitador, VELOCIDADE_AGITADOR_ANTERIOR*200/60); // Manda uma onda quadrada para o driver do motor com a velocidade em rpm desejada.
+      digitalWrite(SLEEP, HIGH);
+      tone(STEP_AGITADOR, VELOCIDADE_AGITADOR_ANTERIOR*200/60); // Manda uma onda quadrada para o driver do motor com a velocidade em rpm desejada.
   }
 }
 
 void exibe_dados()
 {
+  Serial.print("\n************************************************************************\n");
 
   if(CH_HABILITA_AGITADOR)
   {
-    Serial.print(" Velocidade do agitador (rpm): ");
+    Serial.print("Velocidade do agitador (rpm): ");
     Serial.print(VELOCIDADE_AGITADOR_ANTERIOR);
 
-    Serial.print(" intervalo do agitador (s): ");
-    Serial.print(PERIODO_INTERVALO_AGITADOR);
-
+    if(PERIODO_INTERVALO_AGITADOR != 0)
+    {
+      Serial.print(" Intervalo do agitador (s): ");
+      Serial.print(PERIODO_INTERVALO_AGITADOR);
+    }
+    Serial.print("\n");
   }
 
   if(CH_LIGA_BOMBA1)
   {
-    Serial.print(" PWM Bomba 1: ");
+    Serial.print("Bomba 1: ");
     Serial.print(PWM_BOMBA1);
     Serial.print("%");
   }
 
   if(CH_LIGA_BOMBA2)
   {
-    Serial.print(" PWM Bomba 2: ");
-    Serial.print(PWM_BOMBA2);
-    Serial.print("%");
+    Serial.print(" Bomba 2: ");
+    Serial.println(PWM_BOMBA2);
   }
+
 
   if(CH_LIGA_VALVULA)
   {
-    Serial.print(" Abertura da Valvula: ");
+    Serial.print("Valvula: ");
     Serial.print(ABERTURA_VALVULA);
-    Serial.print("%");
+    Serial.println("%");
   }
   if(CH_RESISTENCIA)
   {
-    Serial.print(" Temperatura Resistência: ");
+    Serial.print("Temp. Resistência: ");
     Serial.print(TEMPERATURA_RESISTENCIA);
   }
 
   if(CH_LIBERA_SENSOR_TEMPERATURA)
   {
-    Serial.print(" Temp ºC: ");
-    Serial.print(TEMPERATURA);
+    Serial.print("Temp ºC: ");
+    Serial.println(TEMPERATURA);
   }
+
+  if(CH_HABILITA_SENSOR_PRESSAO)
+  {
+    Serial.print("Volume: ");
+    Serial.print(VOLUME);
+  }
+  
   Serial.println(" ");
 }
